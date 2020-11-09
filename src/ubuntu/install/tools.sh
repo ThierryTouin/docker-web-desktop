44020d4de905e32b29ac9b36cfbee1598f4f9e9b
Thierry Touin <thierrytouin@gmail.com>
2020-11-09 23:37:19 +0100

    first commit

---
+++ src/ubuntu/install/tools.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+### every exit != 0 fails the script
+set -e
+
+echo "Install some common tools for further installation"
+apt-get update 
+apt-get install -y vim wget net-tools locales bzip2 \
+    python python-numpy #used for websockify/novnc
+apt-get clean -y
+
+#echo "generate locales für en_US.UTF-8"
+#locale-gen en_US.UTF-8
+
+echo "generate locales pour fr_FR.UTF-8"
+locale-gen fr_FR.UTF-8
