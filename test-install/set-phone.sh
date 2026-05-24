8d5b112bc6ced7d889a0b1228cbed9796367e50d
Thierry <thierrytouin@gmail.com>
2026-05-24 23:32:32 +0200

    test

--- test-install/set-phone.sh
+++ test-install/set-phone.sh
@@ -2,6 +2,20 @@
 
 echo "=== Configuration pour écran smartphone (scale x2) ==="
 
-xfconf-query -c xsettings -p /Gdk/WindowScalingFactor -s 2
+# Scaling factor GDK (GTK3/GTK4)
+xfconf-query -c xsettings -p /Gdk/WindowScalingFactor -n -t int -s 2
 
-echo "Scaling appliqué : x2"
+# DPI élevé (96 x 2 = 192)
+xfconf-query -c xsettings -p /Xft/DPI -n -t int -s 192
+
+# Variable d'environnement pour les apps GTK en cours
+export GDK_SCALE=2
+export GDK_DPI_SCALE=0.5
+
+echo "Scaling appliqué : x2 (DPI=192)"
+echo ""
+echo "Note : les applications déjà ouvertes peuvent nécessiter"
+echo "un redémarrage pour prendre en compte le changement."
+echo "Pour que GDK_SCALE persiste, ajoutez dans ~/.profile :"
+echo "  export GDK_SCALE=2"
+echo "  export GDK_DPI_SCALE=0.5"
